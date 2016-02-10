defe1eee55fa6c1ad9cb19c0bb1cc0dfe0bd97e2
Melissa M. Dailey <mel.dailey@gmail.com>
2016-02-09 21:16:31 -0800

    final details

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,48 @@
 $(function() {
     //Open panels
+    var windowWidth = $( window ).width();
+
+    var open = 0;
+
+
+
     $('.first').click(function(ev) {
-        var $second = $('.second-panel');
-        $second.animate({right: 0}, 1000);
-        move();
+
+        if (open == 0) {
+
+            var $second = $('.second-panel');
+            if (windowWidth < 767) {
+                $second.animate({right: 0}, 1000);
+                move();
+                open = 1;
+            }
+            else {
+                $second.animate({right: '10%'}, 1000);
+                move();
+                open = 1;
+            }
+        }
+        
     });
     $('.individual-accounts').click(function(ev) {
         var $third = $('.third-panel');
-        $third.animate({right: 0}, 1000);
+        if (windowWidth < 767) {
+            $third.animate({right: 0}, 1000);
+        }
+        else {
+            $third.animate({right: '5%'}, 1000);
+        }
     });
     //Close panels
     $(".second-panel").on("click", ".close-panel", function(e) {
         var $second = $('.second-panel');
         $second.animate({right: '-150%'}, 1000)
+        open = 0;
     });
     $(".third-panel").on("click", ".close-panel", function(e) {
         var $third = $('.third-panel');
-        $third.animate({right: '-150%'}, 1000)
+        $third.animate({right: '-150%'}, 1000);
+        open = 0;
     });
 });
 
