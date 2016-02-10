02027a3613d001556467477265d3d9774e6f9179
Melissa M. Dailey <mel.dailey@gmail.com>
2016-02-10 00:05:51 -0800

    added few styling edits

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,9 @@
 $(function() {
-    //Open panels
     var windowWidth = $( window ).width();
-
     var open = 0;
-
-
-
+    //Open Panel
     $('.first').click(function(ev) {
-
         if (open == 0) {
-
             var $second = $('.second-panel');
             if (windowWidth < 767) {
                 $second.animate({right: 0}, 1000);
@@ -22,7 +16,7 @@ $(function() {
                 open = 1;
             }
         }
-        
+        $(this).css("background", "#d7d7d7");
     });
     $('.individual-accounts').click(function(ev) {
         var $third = $('.third-panel');
@@ -35,6 +29,7 @@ $(function() {
     });
     //Close panels
     $(".second-panel").on("click", ".close-panel", function(e) {
+        $('.first').css("background", "white");
         var $second = $('.second-panel');
         $second.animate({right: '-150%'}, 1000)
         open = 0;
@@ -47,15 +42,12 @@ $(function() {
 });
 
 
-
-
-
 function move() {
     var elem = document.getElementById("bar"); 
     var width = 0;
     var id = setInterval(frame, 50);
     function frame() {
-        if (width == 100) {
+        if (width == 70) {
             clearInterval(id);
         } else {
             width++; 
