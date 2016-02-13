38cfc8c6b6a7a40cb380fc651073e969555e3424
Melissa M. Dailey <mel.dailey@gmail.com>
2016-02-12 16:03:59 -0800

    testing port

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var express = require('express'),
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+app.set('port', (process.env.PORT || 5000));
+
 // views path
 var views = path.join(process.cwd(), "views");
 
@@ -19,6 +21,9 @@ app.get('/', function(req, res) {
 });
 
 // start the server
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
